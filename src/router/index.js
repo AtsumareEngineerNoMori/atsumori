a40e56dc10caef084cc04fbaab1d9fea0493db48
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-04-17 14:12:39 +0900

    編集途中

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TopTest from "../views/TopTest.vue"
+import IslandReqList from "@/views/ReqList/IslandReqList.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/top',
       name: 'top',
       component: TopTest
+    },
+    {
+      path: '/islandReqList',
+      name: 'islandReqList',
+      component: IslandReqList
     },
   ]
 })
---
+++ src/views/ReqList/ IslandReqList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="reqLists">
+    <div class="detail-image">
+    </div>
+    <div class="detail-text">
+      <p class="detail-text-name">aaaaaaa</p>
+      <p class="detail-text-admin">bbbbbb</p>
+    </div>
+  </div>
+</template>
