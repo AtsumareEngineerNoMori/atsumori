6b847b2be8c29a8acd85d25b340caa01014b15a7
takuto terada <takuto.terada@rakus-partners.co.jp>
2023-04-17 14:28:01 +0900

    css追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TopTest from "../views/TopTest.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import TopTest from "../views/TopTest.vue";
+import IslandShow from "../views/Show/IslandShow.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/top',
-      name: 'top',
-      component: TopTest
+      path: "/top",
+      name: "top",
+      component: TopTest,
     },
-  ]
-})
+    {
+      path: "/show",
+      name: "show",
+      component: IslandShow,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/Show/IslandShow.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+  <div class="detail">
+    <div class="detail__image">
+      <img
+        src="../../../public/beach-1824855_1920.jpg"
+        class="detail__image__header"
+      />
+      <img
+        src="../../../public/atsumare engimeer no Mori.png"
+        class="detail__image__icon"
+      />
+
+      <div class="detail__image__setting">※</div>
+    </div>
+
+    <div class="detail__text">
+      <p class="detail__text__name">aaaaaaa</p>
+      <p class="detail__text__admin">bbbbbb</p>
+    </div>
+
+    <div class="detail__btn">
+      <button>移住申請</button>
+    </div>
+
+    <div class="detail__desc">aaaaaaaa</div>
+
+    <div class="detail__member"></div>
+  </div>
+</template>
