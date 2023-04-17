95d3f1ace16e5b273d650bbc646dbaaa1d7d2ab1
Ikuri Kijima <ikuri.kijima@rakus-partners.co.jp>
2023-04-17 14:23:11 +0900

    q

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TopTest from "../views/TopTest.vue"
+import MyPage from "../views/MyPage.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/top',
       name: 'top',
       component: TopTest
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MyPage
     },
   ]
 })
---
+++ src/views/MyPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <h1>マイページ</h1>
+  <button>依頼一覧／スカウトボタン</button>
+  <ul>
+    <li>
+      <span>アイコン<img src="" alt=""></span>
+      <span>アイコン</span>
+    </li>
+    <li>
+      <span>なまえ</span>
+      <span>きじま　いくり</span>
+    </li>
+    <li>
+      <span>職種</span>
+      <span>FR</span>
+    </li>
+    <li>
+      <span>ひとこと</span>
+      <span>あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</span>
+    </li>
+  </ul>
+  <!-- 島一覧 -->
+  <ul>
+    <span>島一覧</span>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <button>・・・</button>
+    </li>
+  </ul>
+
+  <!-- プロジェクト一覧 -->
+  <ul>
+    <span>プロジェクト一覧</span>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <img src="" alt="">
+      <p>プロジェクト名</p>
+    </li>
+    <li>
+      <button>・・・</button>
+    </li>
+  </ul>
+</template>
