09e1ba8a3f25ccb73a475a9b3a6b1f2dfeb0f443
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2023-04-17 11:45:17 +0900

    プルのため

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>ロゴ</div>
+    <div><input /></div>
+    <div><input /></div>
+    <div><button>ログイン</button></div>
+    <RouterLink to="/">アカウントを登録しよう！</RouterLink>
+</template>
+
+<script setup>
+</script>
