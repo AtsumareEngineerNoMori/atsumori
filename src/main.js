a7ac0a4863e7300d8b5528aee7fb4227392baf70
emiri yasuo <emiri.yasuo@rakus-partners.co.jp>
2023-04-17 15:09:32 +0900

    login　スタイル完

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-<<<<<<< HEAD
-import "./assets/main.css";
-import "./css/main.css";
-=======
-import './css/main.css';
 
->>>>>>> 417dd369f8017e430c7264704bd38d211d13651e
+import "./css/main.css";
 
 const app = createApp(App);
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,17 @@
 <template>
-    <img src="../../public/atsumare engimeer no Mori.png" />
-    <div><input /></div>
-    <div><input /></div>
-    <div><button class="login">ログイン</button></div>
-    <RouterLink to="/">アカウントを登録しよう！</RouterLink>
+
+    <div class="loginPage-logo">
+      <img src="../../public/atsumare engimeer no Mori.png" />
+    </div>
+
+    <div class="loginPage-text">
+    <div><input typr="email" placeholder="メールアドレスを入力してください"  class="loginPage-email"/></div>
+    <div><input type="password" placeholder="パスワードを入力してください" class="loginPage-password"/></div>
+    <div><button class="loginPage-button">ログイン</button></div>
+    <div class="loginPage-register-margin"><RouterLink to="/"><span class="loginPage-register">アカウントを登録しよう！</span></RouterLink></div>
+  </div>
 </template>
 
 <script setup>
+import "../css/main.css";
 </script>
