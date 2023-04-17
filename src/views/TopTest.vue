0dbfb592f22f1b7c112c7be5f1460c029c2624fe
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2023-04-17 10:09:44 +0900

    jsonサーバー構築

---
+++ src/views/TopTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+const getFlight = async() => {
+const response = await fetch(
+        `http://localhost:8000/items`
+      );
+      const data = await response.json();
+      console.log(data)
+}
+getFlight();
+
+</script>
