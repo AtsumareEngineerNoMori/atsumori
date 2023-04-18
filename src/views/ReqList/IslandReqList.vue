2dd678da31f0dbc0f0c70100ac6a36cec1cb1063
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-04-18 17:15:44 +0900

    島募集一覧途中

--- src/views/ReqList/IslandReqList.vue
+++ src/views/ReqList/IslandReqList.vue
@@ -1,61 +1,46 @@
-   <!-- 一覧を表示、検索は再検索用 -->
+<!-- 一覧を表示、検索は再検索用 -->
 <template>
   <div class="serch">
     <section>
-      <h3 class="serch_title">島  募集一覧</h3>
+      <h3 class="serch_title">島 募集一覧</h3>
     </section>
 
     <div class="island_serch">
       <form action="どこから取ってくるかURL" method="get">
         <p>キーワードを入力してください。</p>
-        <input type="search" name="search" placeholder="キーワードを入力" class="serch_box" />
+        <input
+          type="search"
+          name="search"
+          placeholder="キーワードを入力"
+          class="serch_box"
+        />
         <input type="submit" name="submit" value="検索" class="serch_btn" />
       </form>
+    </div>
+
+    <!-- 結果 -->
+    <section class="serch_list">
+      <router-link to="/各島の詳細画面">
+      <div v-for="island in islands" :key="island.id">
+        <img :src="island.icon" alt="island" class="serch_iconImg" />
+        <p>{{ island.islandName }}</p>
       </div>
-  <section class="serch_list">
-      <!-- <div v-if="serchImages.length > 0"> -->
-        <div v-for="serchImage in serchImages" :key="serchImage">
-          <router-link to="島詳細ページ" class="serch_link">
-            <img :src="serchImage.icon" alt="island" class="serch_iconImg" />
-            <p>{{ serchImage.name }}</p>
-          </router-link>
-        </div>
-      <!-- </div>
-      <div v-else>
-        <p>検索結果がありません</p>
-      </div> -->
+      </router-link>
     </section>
   </div>
 </template>
 
 <script setup>
-// import "../css/main.css";
+import { onMounted, ref } from "vue";
 
-const serchImages = [
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-  {
-    icon: "https://1.bp.blogspot.com/-4Ng1gNmOhAM/V2ucIdYoIAI/AAAAAAAA7vs/trvOgTP7V30aBo8mAV-d5xlcTyaQHCq3gCLcB/s800/mujintou_kojima.png",
-    name: "島の名前",
-  },
-];
+const islands = ref([]);
+//島
+const fetchIslands = async () => {
+  const response = await fetch(`http://localhost:8000/islands`);
+  const data = await response.json();
+  islands.value = data;
+  console.log(data);
+};
 
+onMounted(fetchIslands);
 </script>
