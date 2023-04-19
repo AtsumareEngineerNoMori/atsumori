85ffec8259477b893c660eb7f9cfe33bfd150761
Ayaka Mizuno <ayaka.mizuno@rakus-partners.co.jp>
2023-04-19 11:58:59 +0900

    参加一覧画面データ取得、loadingコンポーネント作成

--- src/views/JoinIsland.vue
+++ src/views/JoinIsland.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { ref } from "vue";
 import "../css/main.css";
+import Loading from "../components/Loading.vue";
 
 const array = [
   {
@@ -27,22 +29,55 @@ const array = [
     name: "島の名前",
   },
 ];
+const joinList = ref([]);
+const islandData = ref([]);
+const loading = ref(true);
+const getJoinIsland = async () => {
+  const response = await fetch(
+    `http://localhost:8000/joinIslands/?userId=${2}`
+  );
+  const data = await response.json();
+  joinList.value = data;
+  console.log(data);
+};
+getJoinIsland().then(() => {
+  console.log(joinList.value);
+  joinList.value.map(async (island) => {
+    const response = await fetch(
+      `http://localhost:8000/Islands/?id=${island.islandId}`
+    );
+    const data = await response.json();
+    islandData.value.push(data);
+    loading.value = false;
+    console.log(data);
+  });
+  console.log(islandData.value);
+});
 </script>
 
 <template>
-  <div class="join">
-    <section>
-      <p class="join__title">参加している島</p>
-    </section>
-    <section class="join__list">
-      <div v-for="island in array" :key="island" class="join__item">
-        <!-- リンク先変える -->
-        <RouterLink v-bind:to="{ name: 'joinIsland' }">
-          <img v-bind:src="island.icon" alt="island" class="join__iconImg" />
-          <p class="join__name">{{ island.name }}</p>
-        </RouterLink>
-        <button class="join__button">退会</button>
-      </div>
-    </section>
+  <div v-if="loading" class="list">
+    <Loading />
   </div>
+  <template v-else>
+    <div class="list">
+      <section>
+        <p class="list__title">参加している島</p>
+      </section>
+      <section class="list__list">
+        <div v-for="island in islandData" :key="island" class="list__item">
+          <!-- リンク先変える -->
+          <RouterLink v-bind:to="{ name: 'joinIsland' }">
+            <img
+              v-bind:src="island[0].icon"
+              alt="island"
+              class="list__iconImg"
+            />
+            <p class="list__name">{{ island[0].islandName }}</p>
+          </RouterLink>
+          <button class="list__button">退会</button>
+        </div>
+      </section>
+    </div>
+  </template>
 </template>
