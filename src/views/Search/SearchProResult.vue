fd514733aa362ae8779a6a71fea3b59bbabfc9a9
Miyu Hamaoka <miyu.hamaoka@rakus-partners.co.jp>
2023-06-05 14:13:29 +0900

    TSにしました

--- src/views/Search/SearchProResult.vue
+++ src/views/Search/SearchProResult.vue
@@ -26,12 +26,19 @@
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted, watchEffect } from "vue";
+<script setup lang="ts">
+import { ref, onMounted, watchEffect,Ref } from "vue";
 import { useRoute } from "vue-router";
-import TopSearchBox from "../../components/Search/TopSearch/TopSearchBox.vue";
+import TopSearchBox from "@/components/Search/TopSearch/TopSearchBox.vue";
 
-const projects = ref([]);
+//型
+type project = {
+  id: number;
+  icon: string;
+  projectName: string;
+};
+
+const projects: Ref<project[]> = ref([]);
 const route = useRoute();
 
 // URLから検索キーワードを取得して、fetchでデータ取得
